7df027ce7d50c82fc751e0c9559424ca1647f8d6
dyrteg <61887221+hotveyn@users.noreply.github.com>
2022-12-09 15:06:56 +0300

    Create ConsultationsPage.vue

---
+++ src/pages/ConsultationsPage.vue
@@ -0,0 +1,52 @@
+<template>
+<main>
+  <div class="container container-consultation">
+    <div
+        v-if="isLoading"
+        class="loading"
+    >
+      <p>Загрузка</p>
+    </div>
+    <template v-else>
+      <div
+          v-for="consultation in consultations"
+          class="consultation"
+      >
+        {{consultation.id}}
+      </div>
+    </template>
+  </div>
+</main>
+</template>
+
+<script setup lang="ts">
+import IConsultation from "@/interfaces/IConsultation";
+import {baseFormRequest} from "@/services/baseFormRequest";
+import {ref} from "vue";
+
+let consultations = ref<IConsultation[]>();
+let isLoading = ref<boolean>(true);
+
+async function getConsultations(){
+  const request = new(baseFormRequest as any)("http://127.0.0.1:8000/api/consultations/","GET",{},localStorage.getItem("token"))
+  let response = await request.goSend();
+
+  if(response.ok){
+    response = await response.json();
+
+    consultations.value = response.data.consultations;
+    isLoading.value = false;
+
+    return console.log(response.status);
+  }
+
+  return console.log(response.status)
+
+}
+
+getConsultations()
+</script>
+
+<style scoped lang="scss">
+
+</style>
