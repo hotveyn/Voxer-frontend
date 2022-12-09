b18ec3df9888a8f65c7d9877ef5f19bdb4157d36
dyrteg <61887221+hotveyn@users.noreply.github.com>
2022-12-09 15:05:09 +0300

    Create IConsultation.ts

---
+++ src/interfaces/IConsultation.ts
@@ -0,0 +1,14 @@
+import IKid from "@/interfaces/IKid";
+import IQuestion from "@/interfaces/IQuestion";
+import IRequest from "@/interfaces/IRequest";
+import IReview from "@/interfaces/IReview";
+import IUser from "@/interfaces/IUser";
+
+export default interface IConsultation{
+    id:number,
+    kid: IKid,
+    question: IQuestion,
+    consultant: IUser,
+    review: IReview,
+    request: IRequest
+}
