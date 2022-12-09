fbfaaebf7baac6519c7f148bdeaba0818e5d558b
dyrteg <61887221+hotveyn@users.noreply.github.com>
2022-12-09 15:06:53 +0300

    Cтили и навигация в header

--- src/components/header/TheHeader.vue
+++ src/components/header/TheHeader.vue
@@ -1,15 +1,65 @@
 <template>
-<header>
-  header
-</header>
+  <header>
+    <div class="container container-header">
+      <RouterLink
+          class="rt-link"
+          :to="{name:'home'}">
+        <h1>Voxer</h1>
+      </RouterLink>
+      <nav>
+        <ul>
+          <li>
+            <RouterLink
+                class="rt-link"
+                :to="{name:'home'}"
+            >
+              Главная
+            </RouterLink>
+          </li>
+          <li>
+            <RouterLink
+                class="rt-link"
+                :to="{name:'profile'}"
+            >
+              Профиль
+            </RouterLink>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </header>
 </template>
 
-<script>
-export default {
-  name: "TheHeader"
-}
+<script setup lang="ts">
+
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+@import "!/usage.scss";
 
+header {
+  padding: 30px 0;
+  background-color: $myOrange;
+  color: white;
+  margin-bottom: 20px;
+}
+
+
+.rt-link {
+  color: white;
+  font-size: 25px;
+}
+.container-header {
+  @include myFlex();
+  h1 {
+    font-size: 40px;
+  }
+
+  nav {
+    ul {
+      @include myFlex();
+      gap: 20px;
+    }
+  }
+}
 </style>
