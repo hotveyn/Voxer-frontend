cc379e987f907e07f40f48300e511027bac9cd95
dyrteg <61887221+hotveyn@users.noreply.github.com>
2022-12-09 10:51:38 +0300

    Create DefaultLayout.vue

---
+++ src/layouts/DefaultLayout.vue
@@ -0,0 +1,16 @@
+<template>
+<TheHeader/>
+  <slot/>
+<TheFooter/>
+</template>
+
+<script setup lang="ts">
+import TheHeader from "@/components/header/TheHeader.vue"
+import TheFooter from "@/components/footer/TheFooter.vue"
+
+
+</script>
+
+<style scoped>
+
+</style>
