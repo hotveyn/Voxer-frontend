03d079cf21bc2616463229ef744c3eaca5628c7f
dyrteg <61887221+hotveyn@users.noreply.github.com>
2022-12-09 15:06:06 +0300

    Create RegPage.vue

---
+++ src/pages/RegPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <main>
+    <div class="container container-login">
+      <h2>Регистрация</h2>
+      <p>В апи нет роута регистрации а делать сейчас я его не буду</p>
+
+    </div>
+  </main>
+
+</template>
+
+<script>
+export default {
+  name: "RegPage"
+}
+</script>
+
+<style scoped>
+
+</style>
