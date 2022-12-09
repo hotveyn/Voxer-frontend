7827c7e099a062ca00c84254a5fe4593f6489c59
dyrteg <61887221+hotveyn@users.noreply.github.com>
2022-12-09 15:05:29 +0300

    Create baseFormRequest.ts

---
+++ src/services/baseFormRequest.ts
@@ -0,0 +1,26 @@
+export const baseFormRequest = function (this: any, url: string, method: string = "POST", attr: any = {}, token: string = "") {
+    async function goSend() {
+        if (method !== "POST") {
+            return await fetch(url + "?" + new URLSearchParams(attr), {
+                method: method,
+                headers: {
+                    "Accept": "application/json",
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${token}`
+                },
+            });
+        } else {
+            return await fetch(url, {
+                method: method,
+                headers: {
+                    "Accept": "application/json",
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${token}`
+                },
+                body: JSON.stringify(attr)
+            });
+        }
+    }
+
+    return {goSend};
+};
