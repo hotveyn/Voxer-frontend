0cebebda2ef179a3f351f2b98a1a65ea8e193646
dyrteg <61887221+hotveyn@users.noreply.github.com>
2022-12-09 15:05:56 +0300

    Create ButtonForm.vue

---
+++ src/components/forms/ButtonForm.vue
@@ -0,0 +1,40 @@
+<template>
+  <button
+      @click.prevent="redirect()"
+      type="submit"
+      class="button"
+  >
+    <slot/>
+  </button>
+</template>
+
+<script setup lang="ts">
+
+import {useRouter} from "vue-router";
+
+const props = defineProps<{
+  name?: string
+}>();
+
+const router = useRouter();
+
+function redirect(){
+  if(props.name) router.push({name: props.name});
+}
+
+</script>
+
+<style scoped lang="scss">
+@import "!/usage.scss";
+
+.button {
+  background-color: $myOrange;
+  border: 2px solid $myDarkOrange;
+  border-radius: 5px;
+  color: white;
+  cursor: pointer;
+  width: 100%;
+  padding: 10px 15px;
+  font-size: 18px;
+}
+</style>
