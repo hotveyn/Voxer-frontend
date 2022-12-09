308f2c7931117c57d0c4bbfb5c8e301b28845bda
dyrteg <61887221+hotveyn@users.noreply.github.com>
2022-12-09 15:15:00 +0300

    Create ConsultationItem.vue

---
+++ src/components/consultations/ConsultationItem.vue
@@ -0,0 +1,16 @@
+<template>
+<p>Статус - {{ props.consultation.request.status.status }}</p>
+</template>
+
+<script setup lang="ts">
+
+import IConsultation from "@/interfaces/IConsultation";
+
+const props = defineProps<{
+  consultation: IConsultation
+}>();
+</script>
+
+<style scoped lang="scss">
+
+</style>
